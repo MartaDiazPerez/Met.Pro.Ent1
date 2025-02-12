bf3382c41c8d2869116720b1dd63a9d081ddee34
MartaDiazPerez <marta.diazp@edu.uah.es>
2025-02-12 19:47:32 +0100

    Ejercicio Introduccion

---
+++ src/Entrega1/Ejerb/e1/Author.java
@@ -0,0 +1,44 @@
+package Entrega1.Ejerb.e1;
+// Clase Author, zona de atributos
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    // Zona de metodos
+    //Constructor
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    //Getter's y setter's
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    //Otros
+    @Override
+    public String toString() {
+        return "Author[name=" + name + ",email=" + email + ",gender=" + gender + "]";
+    }
+}
+
